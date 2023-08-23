c51aebc6135b6e61e61dbd2f3feb0f6c32b9e6bd
Konstantin Karpov <karpovkottt@gmail.com>
2023-08-23 15:19:22 +1000

    4.1. разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] arg) {
+        for (int i = 1; i < 10; i+=1) {
+            System.out.printf("1 * %d = %d%n", i, i);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] arg) {
+        System.out.println("Konstantin Karpov");
+        System.out.println("25/11/1979");
+    }
+}

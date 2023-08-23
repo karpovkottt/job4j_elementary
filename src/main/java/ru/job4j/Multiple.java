a73ff3d26f37d1bcbf0f60ea664ae17fd370b016
Konstantin Karpov <karpovkottt@gmail.com>
2023-08-23 15:27:57 +1000

    исправление ошибки для 4.1. разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] arg) {
-        for (int i = 1; i < 10; i+=1) {
+        for (int i = 1; i < 10; i += 1) {
             System.out.printf("1 * %d = %d%n", i, i);
         }
     }

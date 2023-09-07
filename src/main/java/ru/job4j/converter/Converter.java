a238b9c274901226f8cf896a4370d370a5389692
Konstantin Karpov <karpovkottt@gmail.com>
2023-09-07 19:58:34 +1000

    0. Что такоеfix: fix variable reassignment. 0.естирование [#159431 #439442]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -23,10 +23,10 @@ public class Converter {
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 euro. Test result : " + passed);
 
-        float in2 = 180;
-        float expected2 = 3;
-        float out2 = Converter.rubleToDollar(in2);
-        boolean passed2 = expected2 == out2;
-        System.out.println("180 rubles are 3 dollars. Test result : " + passed2);
+        in = 180;
+        expected = 3;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("180 rubles are 3 dollars. Test result : " + passed);
     }
 }

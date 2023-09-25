f409cb5cb7355c9ed3821598189e14bcbf1b1747
Konstantin Karpov <karpovkottt@gmail.com>
2023-09-25 12:53:45 +1000

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -16,13 +16,11 @@ public class Converter {
         float dollar = Converter.rubleToDollar(180);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("180 rubles are " + dollar + " dollars.");
-
         float in = 140;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 euro. Test result : " + passed);
-
         in = 180;
         expected = 3;
         out = Converter.rubleToDollar(in);
